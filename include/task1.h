137bf1b020981de41d914aeb1b048b6335078b66
Ramazan <ramazankka220@gmail.com>
2020-09-15 13:05:10 +0300

    first try

---
+++ include/task1.h
@@ -0,0 +1,6 @@
+#pragma once
+double average(double x, double y);
+bool good(double guess, double x);
+double improve(double guess, double x);
+double iter(double guess, double x);
+double calc(double arg);
---
+++ src/main1.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+#include "task1.h"
+
+int main()
+{
+	double result = calc(2.0);
+	printf("Sqrt(%lf)=%lf\n", 2.0, result);
+	return 0;
+}
